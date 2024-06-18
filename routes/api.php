c5840f33b709aeaaf3b3ecfc4ec7fdc1b5d3074e
drashtipatell18 <165013408+drashtipatell18@users.noreply.github.com>
2024-06-18 17:52:26 +0530

    Role Based Middleware added

--- routes/api.php
+++ routes/api.php
@@ -29,36 +29,52 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware(['auth:sanctum', 'OnlyAdmin:admin'])->group(function(){
+  // User
+  Route::get('/user', [UserController::class, 'users'])->name('user');
+  Route::post('/user/insert',[UserController::class,'userInsert'])->name('insert.user');
+  Route::post('/user/update/{id}', [UserController::class, 'userUpdate'])->name('update.user');
+  Route::get('/user/destroy/{id}',[UserController::class,'userDestroy'])->name('destroy.user');
+
+  //Role
+  Route::get('/role', [RoleController::class, 'role'])->name('role');
+  Route::post('/role/store', [RoleController::class, 'roleStore'])->name('role.store');
+  Route::post('/role/update/{id}', [RoleController::class, 'roleUpdate'])->name('role.update');
+  Route::delete('/role/delete/{id}', [RoleController::class, 'roleDestroy'])->name('role.destroy');
+
+  Route::get('/employee', [EmployeeController::class, 'employees'])->name('employee');
+  Route::post('/employee/insert',[EmployeeController::class,'employeeInsert'])->name('insert.employee');
+  Route::post('/employee/update/{id}', [EmployeeController::class, 'employeeUpdate'])->name('update.employee');
+  Route::get('/employee/destroy/{id}',[EmployeeController::class,'employeeDestroy'])->name('destroy.employee');
+
+  // Category
+  Route::post('/category/store', [CategoryController::class, 'storeCategory'])->name('category.store');
+  Route::post('/category/update/{id}', [CategoryController::class, 'categoryUpdate'])->name('update.category');
+  Route::get('/category/destroy/{id}',[CategoryController::class,'categoryDestroy'])->name('destroy.category');
+
+  // Holiday
+  Route::post('/holiday/insert',[HolidayController::class,'holidayInsert'])->name('insert.holiday');
+  Route::post('/holiday/update/{id}', [HolidayController::class, 'holidayUpdate'])->name('update.holiday');
+  Route::get('/holiday/destroy/{id}',[HolidayController::class,'holidayDestroy'])->name('destroy.holiday');
+
+  // Stock
+  Route::post('/stock/store', [StockController::class, 'stockStore'])->name('stock.store');
+  Route::post('/stock/update/{id}', [StockController::class, 'stockUpdate'])->name('stock.update');
+  Route::delete('/stock/delete/{id}', [StockController::class, 'stockDestroy'])->name('stock.destroy');
+
+  // Book
+  Route::post('/book/insert',[BookController::class,'bookInsert'])->name('insert.book');
+  Route::post('/book/update/{id}', [BookController::class, 'bookUpdate'])->name('update.book');
+  Route::get('/book/destroy/{id}',[BookController::class,'bookDestroy'])->name('destroy.book');
+});
+
 // Login
 Route::post('/login', [UserController::class, 'login']);
 
-
-Route::get('/user', [UserController::class, 'users'])->name('user');
-Route::post('/user/insert',[UserController::class,'userInsert'])->name('insert.user');
-Route::post('/user/update/{id}', [UserController::class, 'userUpdate'])->name('update.user');
-Route::get('/user/destroy/{id}',[UserController::class,'userDestroy'])->name('destroy.user');
-
-//Role
-Route::get('/role', [RoleController::class, 'role'])->name('role');
-Route::post('/role/store', [RoleController::class, 'roleStore'])->name('role.store');
-Route::post('/role/update/{id}', [RoleController::class, 'roleUpdate'])->name('role.update');
-Route::delete('/role/delete/{id}', [RoleController::class, 'roleDestroy'])->name('role.destroy');
-
-Route::get('/employee', [EmployeeController::class, 'employees'])->name('employee');
-Route::post('/employee/insert',[EmployeeController::class,'employeeInsert'])->name('insert.employee');
-Route::post('/employee/update/{id}', [EmployeeController::class, 'employeeUpdate'])->name('update.employee');
-Route::get('/employee/destroy/{id}',[EmployeeController::class,'employeeDestroy'])->name('destroy.employee');
-
 Route::get('/holiday', [HolidayController::class, 'Holiday'])->name('holiday');
-Route::post('/holiday/insert',[HolidayController::class,'holidayInsert'])->name('insert.holiday');
-Route::post('/holiday/update/{id}', [HolidayController::class, 'holidayUpdate'])->name('update.holiday');
-Route::get('/holiday/destroy/{id}',[HolidayController::class,'holidayDestroy'])->name('destroy.holiday');
 
 // Category
 Route::get('/category', [CategoryController::class, 'category'])->name('category');
-Route::post('/category/store', [CategoryController::class, 'storeCategory'])->name('category.store');
-Route::post('/category/update/{id}', [CategoryController::class, 'categoryUpdate'])->name('update.category');
-Route::get('/category/destroy/{id}',[CategoryController::class,'categoryDestroy'])->name('destroy.category');
 
 // Stall
 Route::get('/stall', [StallController::class, 'stall'])->name('stall');
@@ -68,9 +84,6 @@ Route::get('/stall/destroy/{id}',[StallController::class,'StallDestroy'])->name(
 
 //Stock
 Route::get('/stock', [StockController::class, 'stock'])->name('stock');
-Route::post('/stock/store', [StockController::class, 'stockStore'])->name('stock.store');
-Route::post('/stock/update/{id}', [StockController::class, 'stockUpdate'])->name('stock.update');
-Route::delete('/stock/delete/{id}', [StockController::class, 'stockDestroy'])->name('stock.destroy');
 
 //Scrap
 Route::get('/scrap', [ScrapController::class, 'scrap'])->name('scrap');
@@ -86,9 +99,6 @@ Route::get('/payment/destroy/{id}',[PaymentController::class,'paymentDestroy'])-
 
 //Book
 Route::get('/book', [BookController::class, 'book'])->name('book');
-Route::post('/book/insert',[BookController::class,'bookInsert'])->name('insert.book');
-Route::post('/book/update/{id}', [BookController::class, 'bookUpdate'])->name('update.book');
-Route::get('/book/destroy/{id}',[BookController::class,'bookDestroy'])->name('destroy.book');
 
 //Sales Order
 Route::get('/salesorder', [SalesOrderController::class, 'salesorder'])->name('salesorder');
